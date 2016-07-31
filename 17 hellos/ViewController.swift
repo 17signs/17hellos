cf8e2b5c5f9a7f3b5fb176aedf554c1a1e751799
17 signs <dev@17signs.com>
2016-07-31 18:04:09 +0200

    Added basic function

--- 17 hellos/ViewController.swift
+++ 17 hellos/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setLabel(nameTextField: UITextField) {
+        nameLabel.text = "Hi \(nameTextField.text!)"
+    }
 
 }
 
